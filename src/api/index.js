55ff27f6e005b64963ee25650043f9470ea1b236
Kevin Paucar <kpaucar05@gmail.com>
2023-02-13 15:04:42 -0600

    started routines and sert files/folders

---
+++ src/api/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'http://fitnesstrac-kr.herokuapp.com/api/routines';
+export const KEY = '8f40908b-0976-48d7-b2a0-754f91ac2cd4';
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { createRoot } from 'react-dom/client';
 
 const App = ()=> {
+  const [ routines, setRoutines ] = useState([]);
+  useEffect(() => {
+  fetch('http://fitnesstrac-kr.herokuapp.com/api/routines')
+  .then(( response => response.json()))
+  .then( routines => setRoutines(routines))
+  }, []);
   return (
     <div>
-      <h1>Front End</h1>
+      <h1>Fitness Tracker</h1>
+      <h2>Routines({routines.length})</h2>
+      <ul>
+        {
+          routines.map( routine => {
+            return(
+              <li key={ routine.id} >
+                {routine.name}({routine.activities.length})
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 };
