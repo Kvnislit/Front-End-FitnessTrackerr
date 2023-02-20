a960bcb472618fce29658c2e26fd3a8feea59f89
Kevin Paucar <kpaucar05@gmail.com>
2023-02-19 19:28:19 -0600

    update deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-
+process.env.DATABASE_URL = 'postgres://juice_box_w9cn_user:onGvjGzWYqg8ekgebhxK4YyE22KjPE68@dpg-cf6jlp4gqg47vk0olv6g-a/juice_box_w9cn';
 
 app.use('/dist', express.static('dist'));
 app.use('/assets', express.static('assets'));
